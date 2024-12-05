2ef59256dfa42d7d37de86ee77d5b50abba56638
2323-code <nekodoshi23@gmail.com>
2024-12-06 01:00:40 +0900

    first commit

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+use dioxus_logger::tracing::{info, Level};
+
+#[derive(Clone, Routable, Debug, PartialEq)]
+enum Route {
+    #[route("/")]
+    About {},
+    #[route("/blog/:id")]
+    Blog { id: i32 },
+}
+
+fn main() {
+    // Init logger
+    dioxus_logger::init(Level::INFO).expect("failed to init logger");
+    info!("starting app");
+    launch(App);
+}
+
+fn App() -> Element {
+    rsx! {
+        Router::<Route> {}
+    }
+}
+
+#[component]
+fn Blog(id: i32) -> Element {
+    rsx! {
+        Link { to: Route::About {}, "Go to counter" }
+        "Blog post {id}"
+    }
+}
+
+#[component]
+fn Home() -> Element {
+    let mut count = use_signal(|| 0);
+
+    rsx! {
+        Link {
+            to: Route::Blog {
+                id: count()
+            },
+            "Go to blog"
+        }
+        div {
+            h1 { "High-Five counter: {count}" }
+            button { onclick: move |_| count += 1, "Up high!" }
+            button { onclick: move |_| count -= 1, "Down low!" }
+        }
+    }
+}
+
+#[component]
+fn About() -> Element {
+    rsx! {
+        div{
+            h1 {"What is RustLadies?"}
+            p {"RustLadiesは、Rustに興味がある女性のためのコミュニティです。"}
+            p {"初心者、熟練者問わず、Rustに興味がある女性ならどなたでもご参加いただけます。"}
+        }
+    }
+}
